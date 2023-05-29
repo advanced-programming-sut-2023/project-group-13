77e88e18dad02f759b5a32609410cc2467b854ef
amirmahdimahmoudi <mahmoudiamirmahdi82@gmail.com>
2023-05-29 18:37:01 +0330

    start

--- src/main/java/controller/MainMenuController.java
+++ src/main/java/controller/MainMenuController.java
@@ -1,14 +1,15 @@
 package controller;
 
+import javafx.scene.control.Alert;
+import javafx.scene.input.MouseEvent;
+import view.LoginMenu;
+import view.MainMenu;
+
 public class MainMenuController {
-    public void showAllMenus() {
-        System.out.println("map menu --> enter map menu");
-        System.out.println("login menu --> logout");
-        System.out.println("sign up menu --> you can go to it from login menu");
-        System.out.println("profile menu --> enter profile menu");
-        System.out.println("run game menu --> run a new game");
-        System.out.println("new game menu --> go to it from RunGameMenu menu with command: start a new game");
-        System.out.println("game menu --> go to it from NewGameMenu with command: start a new game");
-        System.out.println("kingdom menu --> go to it from game menu with command: kingdom menu");
+    public void signIn(MouseEvent mouseEvent) throws Exception {
+
+    }
+    public void logIn(MouseEvent mouseEvent) throws Exception {
+
     }
 }
--- src/main/java/view/MainMenu.java
+++ src/main/java/view/MainMenu.java
@@ -2,59 +2,28 @@ package view;
 
 import controller.ControllerControllers;
 import controller.MainMenuController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 import model.Enums.DataEnumFile;
 import model.Enums.MainMenuCommands;
 import model.Player;
 import model.SaveAndLoadData;
 
+import java.net.URL;
 import java.util.regex.Matcher;
 
-public class MainMenu {
-    private ControllerControllers controllerControllers;
-    private MainMenuController mainMenuController = new MainMenuController() ;
-    private Matcher matcher;
+public class MainMenu extends Application {
 
-    public MainMenu(ControllerControllers controllerControllers) {
-        this.controllerControllers = controllerControllers;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        URL url = MainMenu.class.getResource("/FXML/MainMenu.fxml");
+        GridPane gridPane = FXMLLoader.load(url);
+        Scene scene = new Scene(gridPane);
+        stage.setScene(scene);
+        stage.show();
     }
-
-    public String run() {
-        System.out.println("you are in main menu");
-        System.out.println("type \"help\" to get more information about this menu");
-        String command;
-        while(true) {
-           command = ScannerMatcher.getScanner().nextLine();
-            if ((matcher = ScannerMatcher.getMatcher(command, MainMenuCommands.WHICHMENU.getPattern())) != null) {
-//
-                if (matcher.group("Menu").matches("\\s*profile\\s+menu\\s*")) {
-                    System.out.println("entered profile menu!");
-                    return "profile menu";
-                } else if (matcher.group("Menu").matches("\\s*map\\s+menu\\s*")) {
-                    System.out.println("entered map menu!");
-                    return "map menu";
-                }
-                else System.out.println("enter menu failed : invalid menu name!");
-                // todo complete MainMenu
-            } else if (command.matches("^\\s*logout\\s*$")) {
-                // todo do the work to logout the current user\
-                Player.getCurrentPlayer().setLoggedIn(false);
-                SaveAndLoadData.SaveToJson(Player.getPlayers(), DataEnumFile.PLAYERS.getFileName());
-                return "logout";
-            } else if (command.matches("^\\s*run\\s+a\\s+new\\s+game\\s*$")) {
-                System.out.println("prepare to start a new game!");
-                return "start game";
-
-            } else if (command.matches("^\\s*show\\s+all\\s+menus\\s*")) {
-                mainMenuController.showAllMenus();
-            } else if (command.matches("^help$")) {
-                System.out.println("type \"show all menus\" to see all the menus\n" +
-                        "type logout to go back to the login menu\n" +
-                        "in some menus by typing \"help\" you can get some guides");
-            } else {
-                System.out.println("Main menu: invalid command!");
-            }
-        }
-
-    }
-
 }
